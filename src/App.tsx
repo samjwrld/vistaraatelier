2d7e4a5846ee72e8812ad7f8cd7768bb27547ca9
sam <34920664+samjwrld@users.noreply.github.com>
2026-05-01 19:28:18 +0530

    1a

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'motion/react';
+
 import { GeometricBackground } from './components/GeometricBackground';
 import { Nav } from './components/Nav';
 import { Footer } from './components/Footer';
 import { ScrollToTop } from './components/ScrollToTop';
 import { PageWrapper } from './components/PageWrapper';
+
 import { Home } from './pages/Home';
 import { Projects } from './pages/Projects';
 import { Studio } from './pages/Studio';
@@ -14,37 +16,19 @@ import { Contact } from './pages/Contact';
 import { ProjectDetail } from './pages/ProjectDetail';
 
 const AnimatedRoutes = () => {
-  const location = useLocation();
-  
-  return (
-    <AnimatePresence mode="wait">
-      <div key={location.pathname}>
-        <Routes location={location}>
-          <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-          <Route path="/projects" element={<PageWrapper><Projects /></PageWrapper>} />
-          <Route path="/projects/:slug" element={<PageWrapper><ProjectDetail /></PageWrapper>} />
-          <Route path="/studio" element={<PageWrapper><Studio /></PageWrapper>} />
-          <Route path="/approach" element={<PageWrapper><Approach /></PageWrapper>} />
-          <Route path="/contact" element={<PageWrapper><Contact /></PageWrapper>} />
-        </Routes>
-      </div>
-    </AnimatePresence>
-  );
+const location = useLocation();
+
+return ( <AnimatePresence mode="wait"> <div key={location.pathname}> <Routes location={location}>
+<Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+<Route path="/projects" element={<PageWrapper><Projects /></PageWrapper>} />
+<Route path="/projects/:slug" element={<PageWrapper><ProjectDetail /></PageWrapper>} />
+<Route path="/studio" element={<PageWrapper><Studio /></PageWrapper>} />
+<Route path="/approach" element={<PageWrapper><Approach /></PageWrapper>} />
+<Route path="/contact" element={<PageWrapper><Contact /></PageWrapper>} /> </Routes> </div> </AnimatePresence>
+);
 };
 
 export default function App() {
-  return (
-    <BrowserRouter basename="/vistaraatelier/">
-      <ScrollToTop />
-      <main className="relative min-h-screen font-sans overflow-x-hidden">
-        <GeometricBackground />
-        <Nav />
-        <div className="pt-0">
-          <AnimatedRoutes />
-        </div>
-        <Footer />
-      </main>
-    </BrowserRouter>
-  );
+return ( <HashRouter> <ScrollToTop /> <main className="relative min-h-screen font-sans overflow-x-hidden"> <GeometricBackground /> <Nav /> <div className="pt-0"> <AnimatedRoutes /> </div> <Footer /> </main> </HashRouter>
+);
 }
-
